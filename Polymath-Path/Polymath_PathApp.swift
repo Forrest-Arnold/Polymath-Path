cccb02209c258622ebd0f6de105f6df0f4b3f786
Forrest-Arnold <forkalarn@gmail.com>
2025-04-08 23:03:44 -0700

    Initial Commit

---
+++ Polymath-Path/Polymath_PathApp.swift
@@ -0,0 +1,17 @@
+//
+//  Polymath_PathApp.swift
+//  Polymath-Path
+//
+//  Created by Forrest Kalani Arnold on 4/8/25.
+//
+
+import SwiftUI
+
+@main
+struct Polymath_PathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Polymath-PathTests/Polymath_PathTests.swift
@@ -0,0 +1,16 @@
+//
+//  Polymath_PathTests.swift
+//  Polymath-PathTests
+//
+//  Created by Forrest Kalani Arnold on 4/8/25.
+//
+
+import Testing
+
+struct Polymath_PathTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
